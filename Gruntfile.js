4beff04066981601fb577f5e86cafefd4a51cea5
koukan <son@sonsite.com>
2015-11-09 14:12:25 +0900

    message

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@
  * http://getbootstrap.com
  * Copyright 2013-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * test son
  */
 
 module.exports = function (grunt) {
